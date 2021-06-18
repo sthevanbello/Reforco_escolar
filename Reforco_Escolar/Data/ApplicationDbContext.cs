3f8626d3ae90ab28e46df9a647b142146cbb60c5
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-06-18 14:58:37 -0300

    First commit - Initializing 'Project - Reforço escolar'

---
+++ Reforco_Escolar/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reforco_Escolar.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Reforco_Escolar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Reforco_Escolar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
