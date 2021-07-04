b10ffbc63b5cfade8ceb9e6be06f1c25a9825de3
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-07-04 00:00:15 -0300

    Adaptando os métodos para tela de Login

---
+++ Reforco_Escolar/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Reforco_Escolar.Data;
+
+[assembly: HostingStartup(typeof(Reforco_Escolar.Areas.Identity.IdentityHostingStartup))]
+namespace Reforco_Escolar.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
